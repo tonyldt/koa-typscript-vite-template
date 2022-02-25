719875ec6623c119f00614236f78ef6d60c068cd
Tuan Le <tuanldt@outlook.com>
2022-02-25 23:46:59 +0100

    environment variables

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import Koa from 'koa';
 const app = new Koa();
 
 app.use(async (ctx) => {
-  ctx.body = 'Hello World';
+  ctx.body = `Hello World\nVite port is:${import.meta.env.VITE_TEST_VAR}`;
 });
 
 if (!app) app.listen(3000);
